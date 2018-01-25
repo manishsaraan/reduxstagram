a79c4f77c23dcfb68c2dd1f1f86fa2e5b87fee82
manishsaraan <manish021js@gmail.com>
2018-01-25 23:14:28 +0530

    initial commit

---
+++ client/reduxstagram.js
@@ -0,0 +1,7 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+//import css
+import css from './styles/style.styl';
+render(<h1>Hello World</h1>, document.getElementById('root'));
